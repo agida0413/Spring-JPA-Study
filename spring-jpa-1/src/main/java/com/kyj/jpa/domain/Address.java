c4ca8f0382963d41de13620b2bb2371163c6fd1a
kimyj <yongjjun8718@gmail.com>
2025-09-13 00:09:32 +0900

    day 4 -2

--- spring-jpa-1/src/main/java/com/kyj/jpa/domain/Address.java
+++ spring-jpa-1/src/main/java/com/kyj/jpa/domain/Address.java
@@ -2,13 +2,24 @@ package com.kyj.jpa.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
-import lombok.Setter;
+
 
 @Embeddable
-@Getter @Setter
+@Getter
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    /* 리플렉션 떄문 */
+    protected Address(){
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
